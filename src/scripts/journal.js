a52889c8382991dc21487558bce2f7ebd32d08a3
Caroline Brownlee <brownleecaroline@gmail.com>
2019-07-15 08:51:56 -0500

    made changes to journal

---
+++ src/scripts/journal.js
@@ -0,0 +1,33 @@
+const objectsAndArrays = {
+    date: "July 12, 2019",
+    concepts: "JavaScript Objects and Arrays",
+    content: "Worked with objects and arrays and logging to the console",
+    mood: "confused"
+}
+
+const flexbox= {
+    date: "July 12, 2019",
+    concepts: "Flexbox",
+    content: "I learned to use flexbox to style in CSS.",
+    mood: "happy"
+}
+
+const groupProject1 = {
+    date: "July 12, 2019",
+    concepts: "Group Project",
+    content: "Learned to work in a group to build and design a tribute sight for John Legend.",
+    mood: "happy"
+}
+
+const usingGitHub = {
+    date: "July 12, 2019",
+    concepts: "Using GitHub",
+    content: "We learned to set up repositories and push to master through the command line.",
+    mood: "confused"
+}
+
+const myJournal = []
+myJournal.push(objectsAndArrays)
+myJournal.push(flexbox)
+myJournal.push(groupProject1)
+myJournal.push(usingGitHub)
